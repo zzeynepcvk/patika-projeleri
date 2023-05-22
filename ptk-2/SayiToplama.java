8f77507027bf04214614a6ceb9191937a1af81ec
zeynep çevik <95767424+zzeynepcvk@users.noreply.github.com>
2023-05-22 17:51:11 +0300

    Add files via upload

---
+++ ptk-2/SayiToplama.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class SayiToplama {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int toplam = 0;
+        int girilenSayi;
+
+        do {
+            System.out.print("Bir sayı giriniz (Programdan çıkmak için 0'a basın): ");
+            girilenSayi = scanner.nextInt();
+
+            if (girilenSayi % 2 == 0 && girilenSayi % 4 == 0) {
+                toplam += girilenSayi;
+            }
+        } while (girilenSayi%2 ==0);
+
+        System.out.println("Toplam: " + toplam);
+
+        scanner.close();
+    }
+}
