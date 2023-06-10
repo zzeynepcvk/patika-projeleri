ed54e1083c8f461801f5212376d76f0ba51fd3ab
zeynep çevik <210541009@firat.edu.tr>
2023-06-10 18:07:13 +0300

    siralamaa

---
+++ ptk-28/DiziSiralama.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class DiziSiralama {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Dizinin boyutu n: ");
+        int n = scanner.nextInt();
+        
+        int[] dizi = new int[n];
+        System.out.println("Dizinin elemanlarını giriniz:");
+        for (int i = 0; i < n; i++) {
+            System.out.print((i+1) + ". Elemanı: ");
+            dizi[i] = scanner.nextInt();
+        }
+        
+        Arrays.sort(dizi); // Diziyi küçükten büyüğe sırala
+        
+        System.out.print("Sıralama: ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(dizi[i] + " ");
+        }
+    }
+}
